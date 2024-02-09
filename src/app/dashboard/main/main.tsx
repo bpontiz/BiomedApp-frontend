b77516cb120a5b854234815e29d7839cd12cc54a
bpontiz <bpontiz@gmail.com>
2024-02-09 14:15:53 -0300

    [update] main dashboard with animated icons on cards

--- src/app/dashboard/main/main.tsx
+++ src/app/dashboard/main/main.tsx
@@ -1,20 +1,20 @@
 'use client'
 import { todayIs } from '@/app/lib/date';
-import BuildIcon from '@mui/icons-material/Build';
-import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
-import FlashOffIcon from '@mui/icons-material/FlashOff';
-import ConstructionSharpIcon from '@mui/icons-material/ConstructionSharp';
 import { Player } from '@lordicon/react';
 import styles from './main.module.css';
 import repairIcon from '../../lib/repair.json';
-import { useEffect, useRef } from 'react';
+import lightningIcon from '../../lib/lightning.json';
+import checkIcon from '../../lib/check.json';
+import assignmentIcon from '../../lib/assignment.json';
+import { useRef } from 'react';
 
 
 export function Maindashboard() {
-    const playerRef = useRef<Player>(null);
-    useEffect(() => {
-        playerRef.current?.play();
-    }, []);
+    const playerRefRepair = useRef<Player>(null);
+    const playerRefLightning = useRef<Player>(null);
+    const playerRefCheck = useRef<Player>(null);
+    const playerRefAssignment = useRef<Player>(null);
+
     return (
         <section className={styles.mainSection}>
             <div className={styles.head}>
@@ -22,28 +22,27 @@ export function Maindashboard() {
                 <h4 className={styles.subtitle}>These are your analytics for today <p className={styles.todayIs}>{todayIs()}</p></h4>
             </div>
             <section className={styles.cardContainer}>
-                <div className={styles.card} onMouseEnter={() => playerRef.current?.playFromBeginning()} onMouseLeave={() => playerRef.current?.goToLastFrame()}>
+                <div className={styles.card} onMouseEnter={() => playerRefCheck.current?.playFromBeginning()} onMouseLeave={() => playerRefCheck.current?.play()} >
                     <div className={styles.rowCardData}>
                         <p className={styles.cardStatus}>Repaired</p>
                         <p>Monthly</p>
                     </div>
                     <div className={styles.quantityIconDiv}>
-                        <Player ref={playerRef} size={45} icon={repairIcon} colorize='#286A24' />
-                        {/* <BuildIcon fontSize='large' sx={{color: '#46B041'}} /> */}
+                        <Player ref={playerRefCheck} size={45} icon={checkIcon} colorize='#286A24' />
                         <p className={styles.quantity}>19</p>
                     </div>
                     <div className={styles.rowCardData}>
-                        <p>4%</p>
+                        <p>+4%</p>
                         <p>Since last week</p>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} onMouseEnter={() => playerRefLightning.current?.playFromBeginning()} onMouseLeave={() => playerRefLightning.current?.play()}>
                     <div className={styles.rowCardData}>
                         <p className={styles.cardStatus}>Broken</p>
                         <p>Monthly</p>
                     </div>
                     <div className={styles.quantityIconDiv}>
-                        <FlashOffIcon fontSize='large' sx={{color: '#EF233C'}} />
+                        <Player ref={playerRefLightning} size={45} icon={lightningIcon} colorize='#EF233C' />
                         <p className={styles.quantity}>4</p>
                     </div>
                     <div className={styles.rowCardData}>
@@ -51,13 +50,13 @@ export function Maindashboard() {
                         <p>Since last week</p>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} onMouseEnter={() => playerRefRepair.current?.playFromBeginning()} onMouseLeave={() => playerRefRepair.current?.play()}>
                     <div className={styles.rowCardData}>
                         <p className={styles.cardStatus}>Maintenance</p>
                         <p>Monthly</p>
                     </div>
                     <div className={styles.quantityIconDiv}>
-                        <ConstructionSharpIcon fontSize='large' sx={{color: '#3B88CC'}} />
+                        <Player ref={playerRefRepair} size={45} icon={repairIcon} colorize='#3B88CC' />
                         <p className={styles.quantity}>12</p>
                     </div>
                     <div className={styles.rowCardData}>
@@ -65,13 +64,13 @@ export function Maindashboard() {
                         <p>Since last week</p>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} onMouseEnter={() => playerRefAssignment.current?.playFromBeginning()} onMouseLeave={() => playerRefAssignment.current?.play()}>
                     <div className={styles.rowCardData}>
                         <p className={styles.cardStatus}>In use</p>
                         <p>Daily</p>
                     </div>
                     <div className={styles.quantityIconDiv}>
-                        <AssignmentIndIcon fontSize='large' sx={{color: '#CCB43B'}} />
+                        <Player ref={playerRefAssignment} size={45} icon={assignmentIcon} colorize='#CCB43B' />
                         <p className={styles.quantity}>76</p>
                     </div>
                     <div className={styles.rowCardData}>
